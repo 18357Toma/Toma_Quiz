e7d9f6780e21e6a26fb0dcc290cc790c71758d47
Toma Mirchevska <>
2021-06-02 01:17:42 +0000

    Finished the basic Menupage. Duplicated it and coded DarkMenuPage.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import * #Importing tkinter so I can use the widgets.
 
 #Defining Variables.
 background_color = "mint cream"
-
+background_color2 = "black"
 #Functions.
 
 
@@ -12,7 +12,7 @@ background_color = "mint cream"
 class MenuPage:
     def __init__(self, parent):
       #Setting up the frame.
-      self.quiz_frame = Frame(parent, bg=background_color, padx=80, pady=80)
+      self.quiz_frame = Frame(parent, padx=80, pady=80, bg = background_color)
       self.quiz_frame.grid() #Grid/table structure window.
 
       #Label widget for heading.
@@ -29,7 +29,7 @@ class MenuPage:
       self.var1=IntVar() #Holds the value of radio buttons.
 
       #Start button.
-      self.start_button = Button(self.quiz_frame, text="START", font=("Helvetica", "14", 'bold'), foreground = 'black', bg= '#D9EAD3', highlightthickness=2, highlightbackground='black',  activebackground='light cyan')
+      self.start_button = Button(self.quiz_frame, text="START", font=("Helvetica", "14", 'bold'), foreground = 'black', bg= '#D9EAD3', highlightthickness=2, highlightbackground='black',  activebackground='light gray')
       self.start_button.grid(row=4, column=2, padx=20, pady=30, ipadx=30, ipady=10)
 
       #Dark theme button.
@@ -42,6 +42,37 @@ class MenuPage:
 
 
 
+class DarkMenuPage:
+    def __init__(self, parent):
+      #Setting up the frame.
+      self.quiz_frame = Frame(parent, padx=80, pady=80, bg = background_color2)
+      self.quiz_frame.grid() #Grid/table structure window.
+
+      #Label widget for heading.
+      self.heading_label=Label(self.quiz_frame, text="Mental Health Quiz", font=("Helvetica", "22", "bold"), foreground = 'white', bg = background_color2, highlightthickness=2, highlightbackground='white') #The label is text.
+      self.heading_label.grid(row=1, column=2, padx=10)
+
+      #Subtitle text.
+      self.user_label=Label(self.quiz_frame, text="Hi there! Welcome to the mental health quiz. \n Please enter your name below and press start to begin:", font=("Helvetica","14"), foreground = 'white', bg=background_color2)
+      self.user_label.grid(row=2, column=2, padx=60, pady=20)
+
+      #Enter name box.
+      self.entry_box=Entry(self.quiz_frame)
+      self.entry_box.grid(row=3, column = 2, padx=20, pady=20, ipadx=60, ipady=10)
+      self.var1=IntVar() #Holds the value of radio buttons.
+
+      #Start button.
+      self.start_button = Button(self.quiz_frame, text="START", font=("Helvetica", "14", 'bold'), foreground = 'white', bg= '#775144', highlightthickness=2, highlightbackground='white',  activebackground='light gray')
+      self.start_button.grid(row=4, column=2, padx=20, pady=30, ipadx=30, ipady=10)
+
+      #Dark theme button.
+      self.theme1_button = Button(self.quiz_frame, text="Dark theme", font=("Helvetica","12", 'bold'), foreground = 'white', bg=background_color, padx=10, pady=10, background = "dark gray", highlightthickness=2, highlightbackground='white', activebackground='light gray', relief= FLAT)
+      self.theme1_button.grid(row=0, column=1)
+
+      #Light theme button.
+      self.theme2_button = Button(self.quiz_frame, text="Light theme", font=("Helvetica","12", 'bold'), foreground = 'black', bg=background_color, padx=10, pady=10, background = "white", highlightthickness=2, highlightbackground='white',  activebackground='light gray', relief= FLAT)
+      self.theme2_button.grid(row=0, column=3)
+
 
 #Run Programe.
 if __name__== "__main__": #If this is the file name then it will be able to run.
@@ -49,5 +80,4 @@ if __name__== "__main__": #If this is the file name then it will be able to run.
   root.title("Mental Health Quiz") #Window Title.
   quiz_instance = MenuPage(root) #Making an instance of the class QuizMenu (instantation).
   root.mainloop() #Shows window until user closes it.
-
 
