2f4e82f2dd3759a5b4d7687a841da81a5dcb765b
Toma Mirchevska <84363553+TomaMirchevska@users.noreply.github.com>
2021-06-10 22:34:37 +0000

    Added background images for MenuPage and DarkMenuPage, finished these components for now.

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ username_list = []
 asked = []
 score = 0
 
-
 #Global
 global question_answer
 
@@ -24,10 +23,18 @@ global question_answer
 
 class MenuPage:
   def __init__(self, parent):
+    self.bg_image1 = Image.open("lightbackground.png") 
+    self.bg_image1 = ImageTk.PhotoImage(self.bg_image1)
+    base.configure(bg = background_color) 
     #Setting up the frame.
-    self.quiz_frame = Frame(parent, padx = 40, pady = 40, bg = background_color)
+    self.quiz_frame = Frame(parent)
     self.quiz_frame.grid() #Grid/table structure window.
-    base.geometry("1060x540") #Geometry used to create a fixed window size/window dimensions.
+    base.geometry("1050x600") #Geometry used to create a fixed window size/window dimensions.
+
+    self.image_label= Label(self.quiz_frame, image=self.bg_image1)
+    self.image_label.place(x=0, y=0, relwidth=1, relheight=1) # make label l to fit the parent window always
+
+
     self.bg_image = Image.open("quiztitle.png") #need to use Image if need to resize
     self.bg_image = self.bg_image.resize((500, 100), Image.ANTIALIAS)
     self.bg_image = ImageTk.PhotoImage(self.bg_image)
@@ -36,22 +43,22 @@ class MenuPage:
     self.heading_label.grid(row = 1, column = 2, padx = 10)
 
     #Subtitle text.
-    self.user_label = Label(self.quiz_frame, text="Hi there! Welcome to the mental health quiz. \n Please enter your name below and press start to begin:", font = ("Helvetica","16"),bg = background_color)
+    self.user_label = Label(self.quiz_frame, text="Hi there! \n \n Enter your name below and press start to begin:", font = ("Helvetica","16"), bg = background_color)
     self.user_label.grid(row = 2, column = 2, padx = 60, pady = 40)
 
     #Enter name box.
     self.entry_box = Entry(self.quiz_frame)
-    self.entry_box.grid(row = 3, column = 2, padx = 20, pady = 20, ipadx = 60, ipady = 10)
+    self.entry_box.grid(row = 3, column = 2, padx = 20, pady = 20, ipadx = 80, ipady = 10)
     self.value=IntVar() #Holds the value of radio buttons.
 
 
     #Dark theme button.
-    self.theme1_button = Button(self.quiz_frame, text = "Dark theme", font = ("Helvetica","12", 'bold'), foreground = 'black', bg = background_color, background = "dark gray", highlightthickness = 2, highlightbackground = 'black', activebackground = 'light gray', relief = FLAT, padx = 10, pady = 10, command = self.dark_theme)
-    self.theme1_button.grid(row = 0, column = 1)
+    self.theme1_button = Button(self.quiz_frame, text = "Dark theme", font = ("Helvetica","14", 'bold'), foreground = 'black', bg = background_color, background = "dark gray", highlightthickness = 2, highlightbackground = 'black', activebackground = 'light gray', relief = RAISED, pady = 10, padx = 10, command = self.dark_theme)
+    self.theme1_button.grid(row = 0, column = 1, pady = 20, padx = 20)
 
     #Light theme button.
-    self.theme2_button = Button(self.quiz_frame, text = "Light theme", font = ("Helvetica","12", 'bold'), foreground = 'black', bg = background_color, background = "white", highlightthickness = 2, highlightbackground = 'black',  activebackground = 'light gray', relief = FLAT, padx = 10, pady = 10,)
-    self.theme2_button.grid(row = 0, column = 3)
+    self.theme2_button = Button(self.quiz_frame, text = "Light theme", font = ("Helvetica","14", 'bold'), foreground = 'black', bg = background_color, background = "white", highlightthickness = 2, highlightbackground = 'black',  activebackground = 'light gray', relief = RAISED, pady = 10, padx = 10)
+    self.theme2_button.grid(row = 0, column = 3, pady = 20, padx = 20)
 
     #Start button.
     self.start_button = Button(self.quiz_frame, text = "START", font = ("Helvetica", "18", 'bold'), foreground = 'black', bg= '#D9EAD3', highlightthickness = 2, highlightbackground = 'black',  activebackground = 'light gray', command = self.name_collection)
@@ -76,53 +83,52 @@ class MenuPage:
       elif len(name) >10:
         messagebox.showerror("Name error:", "Please check that you have entered a name up to 10 characters.")
 
-
-
-      
-    #messagebox
-
-
-
 class QuizPage:
   def __init__(self, parent):
-      self.quiz_frame = Frame(parent , bg = background_color , padx = 80, pady = 80)
+      self.quiz_frame = Frame(parent)
       self.quiz_frame.grid()
 
 
 class DarkMenuPage:
   def __init__(self, parent):
+      self.bg_image1 = Image.open("darkbackground.png") 
+      self.bg_image1 = ImageTk.PhotoImage(self.bg_image1)
+      base.configure(bg = background_color2) 
       #Setting up the frame.
-      self.quiz_frame = Frame(parent, padx = 40, pady = 40, bg = background_color2)
+      self.quiz_frame = Frame(parent)
       self.quiz_frame.grid() #Grid/table structure window.
-      base.geometry("1060x540")
+      base.geometry("1050x600") #Geometry used to create a fixed window size/window dimensions.
+
+      self.image_label= Label(self.quiz_frame, image=self.bg_image1)
+      self.image_label.place(x=0, y=0, relwidth=1, relheight=1) # make label l to fit the parent window always
 
       #Label widget for heading.
       self.bg_image = Image.open("darkquiztitle.png") #need to use Image if need to resize
-      self.bg_image = self.bg_image.resize((500, 90), Image.ANTIALIAS)
+      self.bg_image = self.bg_image.resize((500, 100), Image.ANTIALIAS)
       self.bg_image = ImageTk.PhotoImage(self.bg_image)
       #Label widget for heading.
       self.heading_label = Label(self.quiz_frame, image=self.bg_image, borderwidth = 0) #The label is text.
       self.heading_label.grid(row = 1, column = 2, padx = 10)
 
-    #Subtitle text.
-      self.user_label = Label(self.quiz_frame, text = "Hi there! Welcome to the mental health quiz. \n Please enter your name below and press start to begin:", font = ("Helvetica","16"), foreground = 'white', bg = background_color2)
+      #Subtitle text.
+      self.user_label = Label(self.quiz_frame, text = "Hi there! \n \n Enter your name below and press start to begin:", font = ("Helvetica","16"), foreground = 'white', bg = background_color2)
       self.user_label.grid(row = 2, column = 2, padx = 60, pady = 40)
 
       #Enter name box.
       self.entry_box = Entry(self.quiz_frame)
-      self.entry_box.grid(row = 3, column = 2, padx = 20, pady = 20, ipadx = 60, ipady = 10)
+      self.entry_box.grid(row = 3, column = 2, padx = 20, pady = 20, ipadx = 80, ipady = 10)
       self.value = IntVar() #Holds the value of radio buttons.
 
       #Dark theme button.
-      self.theme1_button = Button(self.quiz_frame, text = "Dark theme", font = ("Helvetica","12", 'bold'), foreground = 'white', bg = background_color, background = "dark gray", highlightthickness = 2, highlightbackground = 'white', activebackground = 'light gray', relief = FLAT, padx = 10, pady = 10)
-      self.theme1_button.grid(row = 0, column = 1)
+      self.theme1_button = Button(self.quiz_frame, text = "Dark theme", font = ("Helvetica","14", 'bold'), foreground = 'white', bg = background_color, background = "dark gray", highlightthickness = 2, highlightbackground = 'white', activebackground = 'light gray', relief = RAISED, padx = 10, pady = 10)
+      self.theme1_button.grid(row = 0, column = 1, pady = 20, padx = 20)
 
       #Light theme button.
-      self.theme2_button = Button(self.quiz_frame, text = "Light theme", font = ("Helvetica","12", 'bold'), foreground = 'black', bg = background_color, background = "white", highlightthickness = 2, highlightbackground = 'white',  activebackground = 'light gray', relief = FLAT, padx = 10, pady = 10, command = self.light_theme)
-      self.theme2_button.grid(row = 0, column = 3)
+      self.theme2_button = Button(self.quiz_frame, text = "Light theme", font = ("Helvetica","14", 'bold'), foreground = 'black', bg = background_color, background = "white", highlightthickness = 2, highlightbackground = 'white',  activebackground = 'light gray', relief = RAISED, padx = 10, pady = 10, command = self.light_theme)
+      self.theme2_button.grid(row = 0, column = 3, pady = 20, padx = 20)
     
       #Start button.
-      self.start_button = Button(self.quiz_frame, text = "START", font = ("Helvetica", "18", 'bold'), foreground = 'white', bg = '#775144', highlightthickness = 2, highlightbackground = 'white',  activebackground = 'light gray', command = self.name_collection)
+      self.start_button = Button(self.quiz_frame, text = "START", font = ("Helvetica", "18", 'bold'), foreground = 'white', bg = '#775144', highlightthickness = 2, highlightbackground = 'white',  activebackground = 'light gray', relief = RAISED, command = self.name_collection)
       self.start_button.grid(row = 4, column = 2, padx = 20, pady = 40, ipadx = 30, ipady = 10)
 
 
